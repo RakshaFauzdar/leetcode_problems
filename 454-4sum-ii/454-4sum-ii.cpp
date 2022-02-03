40c97a270e1b78b1ec8e2d46946eab2ebf97f07e
Raksha Fauzdar <71915465+RakshaFauzdar@users.noreply.github.com>
2022-02-03 10:44:22 +0530

    Time: 300 ms (51.14%), Space: 24.5 MB (49.44%) - LeetHub

--- 454-4sum-ii/454-4sum-ii.cpp
+++ 454-4sum-ii/454-4sum-ii.cpp
@@ -1,15 +1,35 @@
 class Solution {
 public:
     int fourSumCount(vector<int>& nums1, vector<int>& nums2, vector<int>& nums3, vector<int>& nums4) {
+        int c=0;
+        // for(auto i:nums1)
+        // {
+        //     for(auto j:nums2)
+        //     {
+        //         for(auto k:nums3)
+        //         {
+        //             // for(auto l:nums4)
+        //             // {
+        //                 if(i+j+k+==0)
+        //                 {
+        //                     c++;
+        //                 }
+        //             // }
+        //         }
+        //     }
+        // }
+        // return c;
         unordered_map<int,int>m;
         for(auto a:nums1)
         {
+            // m[a]++;
             for(auto b:nums2)
             {
                 m[a+b]++;
             }
         }
         int ans=0;
+        
         for(auto c:nums3)
         {
             for(auto d:nums4)
@@ -18,6 +38,7 @@ public:
                 
             }
         }
+        
         return ans;
         
     }
