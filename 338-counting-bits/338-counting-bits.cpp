31d6733bd95f86c1049375ece7e0048589a86225
Raksha Fauzdar <71915465+RakshaFauzdar@users.noreply.github.com>
2022-06-16 00:22:39 +0530

    Time: 9 ms (47.12%), Space: 8.4 MB (34.02%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,45 +1,11 @@
 class Solution {
 public:
-    int dec(int N)
-{
- 
-    // To store the binary number
-    unsigned long long  B_Number = 0;
-    int cnt = 0;
-    while (N != 0) {
-        int rem = N % 2;
-        unsigned long long c = pow(10, cnt);
-        B_Number += rem * c;
-        N /= 2;
- 
-        // Count used to store exponent value
-        cnt++;
-    }
- 
-    return B_Number;
-}
-int co( int n)
-{
-     int count = 0;
-    while (n) {
-        count += n & 1;
-        n >>= 1;
-    }
-    return count;
-}
- 
     vector<int> countBits(int n) {
-        int a[n+1];
-        for(int i=0;i<n+1;i++)
-        {
-            a[i]=i;
-        }
         vector<int>v;
-        for(int i=0;i<n+1;i++)
+        for(int i=0;i<=n;i++)
         {
-           int c=co(a[i]);
-            v.push_back(c);
-            
+            int x=__builtin_popcount(i);
+            v.push_back(x);
         }
         return v;
         
