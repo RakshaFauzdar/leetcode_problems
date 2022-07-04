afe41be18ea46084e25b64529f6a35b0ed6da80a
Raksha Fauzdar <71915465+RakshaFauzdar@users.noreply.github.com>
2022-07-04 15:06:24 +0530

    Time: 41 ms (31.55%), Space: 17.7 MB (84.26%) - LeetHub

---
+++ 135-candy/135-candy.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n=ratings.size();
+        vector<int>num(n,1);
+        for(int i=1;i<n;i++)
+        {
+            if(ratings[i]>ratings[i-1])
+            {
+                num[i]=num[i-1]+1;
+            }
+        }
+        for(int i=n-1;i>0;i--)
+        {
+            if(ratings[i-1]>ratings[i])
+            {
+                num[i-1]=max(num[i]+1,num[i-1]);
+            }
+            
+        }
+        int c=0;
+        for(int i=0;i<n;i++)
+        {
+            c+=num[i];
+        }
+        return c;
+    }
+};
