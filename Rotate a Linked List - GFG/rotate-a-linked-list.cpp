7b449e71217d88e648279d6bc42bc542b17e08b1
Raksha Fauzdar <71915465+RakshaFauzdar@users.noreply.github.com>
2022-07-13 08:47:46 +0530

    Added solution - LeetHub

---
+++ Rotate a Linked List - GFG/rotate-a-linked-list.cpp
@@ -0,0 +1,122 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+ // } Driver Code Ends
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+
+class Solution
+{
+    public:
+    //Function to rotate a linked list.
+    Node*reverse(Node*head)
+    {
+        Node*curr=head;
+        Node*prev=NULL;
+        Node*next=NULL;
+        while(curr)
+        {
+            next=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+    Node* rotate(Node* head, int k)
+    {
+        Node*curr=head;
+        int c=1;
+         Node*prev=head;
+         Node*next=NULL;
+        while(curr && c<k)
+        {
+            curr=curr->next;
+            c++;
+            
+            
+        }
+       Node*kthNode=curr;
+       while(curr->next)
+       {
+           curr=curr->next;
+       }
+       curr->next=head;
+       head=kthNode->next;
+       kthNode->next=NULL;
+       return head;
+       
+        
+        
+       
+       
+       
+    
+    }
+};
+    
+
+
+// { Driver Code Starts.
+
+void printList(Node *n)
+{
+    while (n != NULL)
+    {
+        cout<< n->data << " ";
+        n = n->next;
+    }
+    cout<< endl;
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, val, k;
+        cin>>n;
+        
+        cin>> val;
+        Node *head = new Node(val);
+        Node *tail = head;
+        
+        for(int i=0; i<n-1; i++)
+        {
+            cin>> val;
+            tail->next = new Node(val);
+            tail = tail->next;
+        }
+        
+        cin>> k;
+        
+        Solution ob;
+        head = ob.rotate(head,k);
+        printList(head);
+    }
+    return 1;
+}
+  // } Driver Code Ends
