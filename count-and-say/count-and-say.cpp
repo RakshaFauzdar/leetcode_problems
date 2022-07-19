8c9947bf7db4f5e60ca9872d82e0c4991bdbef71
Raksha Fauzdar <71915465+RakshaFauzdar@users.noreply.github.com>
2022-07-19 22:05:20 +0530

    Time: 18 ms, Memory: 6.5 MB - LeetHub

---
+++ count-and-say/count-and-say.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    string countAndSay(int n) {
+        string s="1";
+        n=n-1;
+        while(n--)
+        {
+            string ans="";
+            for(int i=0;i<s.size();i++)
+            {
+                int c=1;
+               
+                while((i+1<s.size()) && (s[i]==s[i+1]))
+                {
+                    
+                    c++;
+                    i++;
+                }
+                ans+=to_string(c)+s[i];
+            }
+            s=ans;
+        }
+        return s;
+        
+
+        
+    }
+};
