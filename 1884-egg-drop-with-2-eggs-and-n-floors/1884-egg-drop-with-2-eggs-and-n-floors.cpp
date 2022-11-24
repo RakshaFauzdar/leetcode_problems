2dc7ea1d41610909fbdd5a0e03c5e73322e6110d
Raksha Fauzdar <71915465+RakshaFauzdar@users.noreply.github.com>
2022-11-24 11:26:07 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (61.28%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.cpp
@@ -1,37 +1,15 @@
 class Solution {
 public:
-    int dp[3][1001];
-    // memset(dp,-1,sizeof(dp));
-    int solve(int e,int f)
-    {
-            // memset(dp,-1,sizeof(dp));
-
-        if(f==0 || f==1)
-        {
-            return f;
-        }
-        
-        if(e==1)
-        {
-            return f;
-        }
-        if(dp[e][f]!=-1)
-        {
-            return dp[e][f];
-        }
-        
-        int ans=INT_MAX;
-        for(int i=1;i<=f;i++)
-        {
-            int x=1+max(solve(e-1,i-1),solve(e,f-i));
-                ans=min(x,ans);
-        }
-        return dp[e][f]=ans;
-    }
     int twoEggDrop(int n) {
         
-        memset(dp,-1,sizeof(dp));
-        return solve(2,n);
         
+        int d=1;
+        while(n>0)
+        {
+            n=n-d;
+            d++;
+        }
+        
+        return d-1;
     }
 };
